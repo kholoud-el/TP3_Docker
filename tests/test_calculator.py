a314d48fd86a159e9b47b613f93dd47e31b7f707
kholoud-el <aoulakholoud@gmail.com>
2026-03-17 20:40:15 +0000

    Ajouter les fichiers

---
+++ tests/test_calculator.py
@@ -0,0 +1,12 @@
+import pytest
+from app.calculator import addition, multiplication
+
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+
+def test_multiplication():
+    assert multiplication(2, 3) == 6
+    assert multiplication(-2, 3) == -6
+    assert multiplication(0, 0) == 0
